72c30a176b78f4f38b9d11055d9b328ddc7e558e
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-04-27 20:26:05 +0300

    Add new methods for group manipulation and report generation
    - Changed `animalMap` to be a map of pointers to `Animal` instances instead of map of `Animal` instances.
    - Added `addAnimal` method for adding `Animal` to `AnimalGroup`.
    - Added `removeAnimal` method for removing `Animal` from `AnimalGroup`.
    - Added `mergeGroup` method to merge the current `AnimalGroup` instance into another `AnimalGroup`.
    - Added `generateReport` method to generate a list of `Animal` instances from the group, ordered alphabetically.

--- include/AnimalGroup.h
+++ include/AnimalGroup.h
@@ -10,7 +10,7 @@ class AnimalGroup {
    int groupID;
    int animalAmount;
    std::string groupName;
-   std::unordered_map<int, Animal> animalMap;
+   std::unordered_map<int, Animal*> animalMap;
 
    public: 
    AnimalGroup (const std::string& groupName);
@@ -21,6 +21,13 @@ class AnimalGroup {
 
    std::string getGroupName() const;
 
+   void addAnimal(Animal* newAnimal);
+   
+   void removeAnimal(Animal* removedAnimal);
+
+   void mergeGroup(AnimalGroup& otherGroup); // Merges this group to otherGroup. This group becomes empty
+
+   std::string generateReport() const; // Generates a list of animals from the group in alphabetical order
 };
 
 #endif
--- src/AnimalGroup.cpp
+++ src/AnimalGroup.cpp
@@ -2,6 +2,9 @@
 #include "Animal.h"
 #include <string>
 #include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <algorithm>
 
 using std::string;
 
@@ -16,4 +19,40 @@ int AnimalGroup::getGroupID() const { return groupID; }
 int AnimalGroup::getAnimalAmount() const { return animalAmount; }
 
 string AnimalGroup::getGroupName() const { return groupName; }
+
+void AnimalGroup::addAnimal(Animal* newAnimal) {
+   animalMap.insert({newAnimal->getAnimalID(), newAnimal});
+   ++animalAmount;
+}
+
+void AnimalGroup::removeAnimal(Animal* removedAnimal) {
+   animalMap.erase(removedAnimal->getAnimalID());
+   --animalAmount;
+}
+
+void AnimalGroup::mergeGroup(AnimalGroup& otherGroup) { 
+   for(const auto& [keyID, animal] : this->animalMap) {
+      auto result = otherGroup.animalMap.insert({keyID, animal});
+      if(result.second) ++(otherGroup.animalAmount);
+   }
+   this->animalMap.clear();
+   this->animalAmount = 0;
+}
+
+string AnimalGroup::generateReport() const {
+   std::vector<Animal*> animalList;
+   
+   for(const auto& [keyID, animalPtr] : animalMap) {
+      animalList.push_back(animalPtr);
+   }
+   
+   std::sort(animalList.begin(), animalList.end(),
+      [](Animal* a, Animal* b) { *a < *b; });
+   
+   std::stringstream ss;
+   for(const Animal *animal : animalList) {
+      ss << animal->getAnimalInfo() << std::endl;
+   }
+   return ss.str();
+}
 
