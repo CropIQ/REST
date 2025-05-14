6ab25f935d6cc63dcdb8cc6a74c32f31090ef442
Rokas Braidokas <76654333+MaRokas2005@users.noreply.github.com>
2025-05-14 18:09:01 +0300

    Update Order.h

--- include/Order.h
+++ include/Order.h
@@ -1,30 +1,21 @@
 #pragma once
 
 #include <vector>
+#include <string>
 #include "Item.h"
 #include <nlohmann/json.hpp>
 
 class Order {
 private:
     int id;
-    int orderDate;
-    std::vector<Item> orderContent;
+    std::string orderDate;
+    int itemId;
     int quantity;
-
+    
 public:
-    Order(const int& id, const time_t& orderDate, const int& quantity);
+    Order();
     Order(const nlohmann::json& json);
     ~Order();
 
-    int createOrder(const Item& item, int quantity);
-    void cancelOrder(int id);
-    std::vector<Order> viewOrderHistory() const;
-    std::vector<Order> getPendingOrders() const;
     nlohmann::json toJson() const;
-
-    int getId() const;
-    time_t getOrderDate() const;
-    std::vector<Item> getOrderContent() const;
-    int getQuantity() const;
-
 };
