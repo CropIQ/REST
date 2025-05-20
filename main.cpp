b653799a7f211160683a2d2cf96ae22fde1ccd35
Ugnius Vaičeskas <ugnius@venderes.com>
2025-05-20 15:58:52 +0300

    Remove redundant main routes

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main()
 
     //define your endpoint at the root directory
     CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+        return "Hello";
     });
 
     CROW_ROUTE(app, "/simpleAuth")
@@ -64,28 +64,6 @@ int main()
 
         users["users"] = move(usersList);
         return crow::response{200, users};
-    });
-
-    CROW_ROUTE(app, "/authentication/sayHello/<string>")
-    .methods("GET"_method)
-    ([](string name){
-        //Return a JSON response with 200 status code
-        crow::json::wvalue x;
-        x["message"] = "Hello " + name;
-        x["name"] = name;
-        x["status"] = "success 200";
-        return crow::response{200, x};
-    });
-
-    CROW_ROUTE(app, "/authentication/sayHello/<string>")
-    .methods("POST"_method)
-    ([](string name){
-        //Return a JSON response with 201 status code
-        crow::json::wvalue x;
-        x["message"] = "Hello " + name;
-        x["name"] = name;
-        x["status"] = "success 201";
-        return crow::response{201, x};
     });
 
     app.port(18080).multithreaded().run();
